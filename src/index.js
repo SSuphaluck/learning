1a177865829d7c4fe2f4cdb3dfbd93277874692c
SSuphaluck <hibara.and.conun@gmail.com>
2023-02-12 20:49:01 +0700

    finish learning component and props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Welcome(props) {
+  const initialWelcomeValue = 700;
+  // don't do this
+  // props.age = 20;
+  console.log('Welcome Component');
+  return (
+    <h1>
+      Welcome {props.name}, {props.age} years old.
+    </h1>
+  );
+}
+
+class WelcomeClass extends Component {
+  render() {
+    return <h1>Welcome Class</h1>;
+  }
+}
+
+function Input() {
+  return <input name="component-input" />;
+}
+
+class Position extends React.Component {
+  render() {
+    return <div>Position Component</div>;
+  }
+}
+
+function MainContainer(props) {
+  // Don't modify props [props.gender = 'Male']
+  // pure function is mean never modify owns props
+  const birthDate = '1995-01-06';
+  console.log(props.test); // Array
+  console.log(props.obj); // Obj. for example props.obj.name, props.obj.surname
+  console.log(props.boo); // true
+  return (
+    <div>
+      {/* uniderectional data flow: data sent from Parent to Child only */}
+      <Welcome
+        name="Bob"
+        age="55"
+        birthDate={birthDate}
+      />
+      <Position />
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <MainContainer
+      test={[1, 2, 5]}
+      obj={{ name: 'Pete', surname: 'Jadon' }}
+      boo // มีค่าเป็น true
+    />
+    {/* <WelcomeClass />
+    <Position /> */}
+    {/* {Welcome({ name: 'John' })} */}
+    {/* <Welcome
+      name="John"
+      age="30"
+    /> */}
+    {/* {Welcome({name: 'John', age: '30'})} */}
+    {/* <Welcome name="Mike" />
+    <Welcome name="Peter" />
+    <Welcome name="Sarah" />
+    <Welcome name="Sam" />
+    <Input />
+    <input name="element-input" /> */}
+    {/* {Welcome({ name: 'Mike' })}
+    {Welcome({ name: 'Peter' })}
+    {Welcome({ name: 'Sarah' })}
+    {Welcome({ name: 'Same' })} */}
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
