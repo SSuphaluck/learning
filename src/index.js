0089d5d826d245493253c49dfe793362f1fc6258
SSuphaluck <hibara.and.conun@gmail.com>
2023-02-12 18:35:03 +0700

    lab1-2

--- src/index.js
+++ src/index.js
@@ -4,12 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// Lab 1
+// const fullName = 'Jack Son';
+// const birthYear = '1995';
+// const element = (
+//   <div>
+//     <h1>{fullName}</h1>
+//     <p>{birthYear}</p>
+//   </div>
+// );
+
+// Lab 2
+const height = 1.54;
+const weight = 60;
+const calcBMI = (h, w) => w / h ** 2;
+const element = (
+  <div>
+    {' '}
+    <h1>{calcBMI(height, weight)}</h1>{' '}
+  </div>
 );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
