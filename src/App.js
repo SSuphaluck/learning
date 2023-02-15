35e5c961e872047a22aba4732816b9ddcf761fb8
SSuphaluck <hibara.and.conun@gmail.com>
2023-02-15 11:27:53 +0700

    break

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import './index.css';
+import classes from './App.module.css';
+
+// CSS Style
+//1. inline CSS
+//2. External style sheet
+//3. CSS Module
+//4. CSS in JS
+
+const style = {
+  width: '175px',
+  height: '150px',
+  border: '3px solid red'
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // 1. inline CSS
+    // <div style={style}>
+    //   <h1 style={{ wigth: '100px', height: '100px', border: '2px solid pink' }}>
+    //     Inline CSS
+    //   </h1>
+    // </div>
+
+    //2. external CSS
+    // <div className="w-200 h-100 border-green-2 border-red-4">External CSS</div>
+
+    //3. CSS Module
+    <div className={classes.test}>CSS Module</div>
   );
 }
 
