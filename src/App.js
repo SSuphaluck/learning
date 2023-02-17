efd078487ed33e441e41650e50d1090e94b48c3d
SSuphaluck <hibara.and.conun@gmail.com>
2023-02-17 19:48:03 +0700

    finish learning form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [select, setSelect] = useState('Nonthaburi');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select
+        onChange={(event) => setSelect(event.target.value)}
+        value={select}
+      >
+        <option>Bangkok</option>
+        <option>Nonthaburi</option>
+        <option>Nan</option>
+      </select>
+
+      <input
+        type="text"
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
+      {/* เวลาจะ handle อะไรก็ตามที่อยู่ใน form จะต้องเขียน value และ onChange ให้กับมันเสมอ  */}
+      <input
+        type="file"
+        onChange={(event) => console.log(event.target.files)}
+      />
+      <textarea rows="10" />
     </div>
   );
 }
