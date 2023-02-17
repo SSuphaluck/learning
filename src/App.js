4bbb30df73033a0f0f4a290b3731c5f59910ec64
SSuphaluck <hibara.and.conun@gmail.com>
2023-02-17 18:28:37 +0700

    finish learning list key

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+const students = [
+  'Bic',
+  'Job',
+  'May',
+  'Leelee',
+  'First',
+  'Best',
+  'Way',
+  'Kaew',
+  'Mild',
+  'Ohm',
+  'Ohm'
+];
+
+const data = [
+  { id: 1, name: 'Coke', price: 20, stock: 100, description: 'description1' },
+  { id: 2, name: 'Pepsi', price: 20, stock: 100, description: 'description2' },
+  { id: 3, name: 'Fanta', price: 20, stock: 100, description: 'description3' }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div>
+    //   <p>{students[0]}</p>
+    //   <p>{students[1]}</p>
+    //   <p>{students[2]}</p>
+    //   <p>{students[3]}</p>
+    //   {[<p>Bic</p>, <p>Job</p>, <p>May</p>, <p>Leelee</p>]}
+    // </div>
+
+    // <div>
+    //   {students.map((item, index) => (
+    //     <p key={index}>{item}</p>
+    //   ))}
+    // </div>
+
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>
+          <h3>{item.name}</h3>
+          <p>{item.price}</p>
+          <p>{item.description}</p>
+        </div>
+      ))}
+      {data.map(({ id, name, price, description }) => (
+        <div key={id}>
+          <h3>{name}</h3>
+          <p>{price}</p>
+          <p>{description}</p>
+        </div>
+      ))}
+      {/* <div>
+        <h3>Coke</h3>
+        <p>20</p>
+      </div>
+      <div>
+        <h3>Pepsi</h3>
+        <p>20</p>
+      </div>
+      <div>
+        <h3>Fanta</h3>
+        <p>20</p>
+      </div> */}
     </div>
   );
 }
